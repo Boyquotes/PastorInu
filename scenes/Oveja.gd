9e62708406b5b63f409e9d8546a58934cbf9a138
DiegoRutolo <diegorutolo@gmail.com>
2020-08-02 15:50:29 +0200

    Las ovejas se mueven un poco

---
+++ scenes/Oveja.gd
@@ -0,0 +1,28 @@
+extends KinematicBody
+
+export(int) var speed := 15
+export(int) var accel := 50
+export(float) var limite_exterior = 5.0
+export(float) var limite_interior = 1.0
+
+var velocidad := Vector3.ZERO
+var direccion := Vector3.RIGHT setget set_direccion
+
+
+func _physics_process(delta):
+	# Detectar ovejas
+	for obeja in $DetectorOvejas.get_overlapping_bodies():
+		if translation.distance_to(obeja.translation) > limite_exterior:
+			velocidad = velocidad.move_toward(translation.direction_to(obeja.translation)*speed, accel)
+		elif translation.distance_to(obeja.translation) < limite_interior:
+			velocidad = velocidad.move_toward(obeja.translation.direction_to(translation)*speed, accel)
+	
+	# Detectar perro
+	for perro in $DetectorPerro.get_overlapping_bodies():
+		velocidad = velocidad.move_toward(perro.translation.direction_to(translation)*speed, accel)
+	
+	velocidad = move_and_slide(velocidad, Vector3.UP)
+
+func set_direccion(val: Vector3):
+	direccion = val.normalized()
+
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,19 +3,19 @@ extends KinematicBody
 export(int) var speed := 30
 export(int) var accel := 100
 
-var velocidad := Vector3.ZERO
-var destino := Vector3.ZERO setget set_destino
-var dir := Vector3.RIGHT setget set_direccion
+onready var velocidad := Vector3.ZERO
+onready var direccion := Vector3.RIGHT setget set_direccion
+onready var destino := translation setget set_destino
 
 
 func _physics_process(delta):
-	self.dir = translation.direction_to(destino)
-	velocidad = velocidad.move_toward(dir*speed, accel)
-	velocidad = move_and_slide(velocidad)
+	self.direccion = translation.direction_to(destino)
+	velocidad = velocidad.move_toward(direccion*speed, accel)
+	velocidad = move_and_slide(velocidad, Vector3.UP)
 	
 
 func set_destino(val: Vector3):
 	destino = val
 
 func set_direccion(val: Vector3):
-	dir = val.normalized()
+	direccion = val.normalized()
